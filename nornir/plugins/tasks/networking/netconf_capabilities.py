c0617c47b30ac70e0b48e185c034981c6897d8a4
Patrick Ogenstad <patrick@ogenstad.com>
2019-10-06 12:56:03 +0200

    Add netconf_capabilities plugin

--- nornir/plugins/tasks/networking/netconf_capabilities.py
+++ nornir/plugins/tasks/networking/netconf_capabilities.py
@@ -2,13 +2,9 @@ from nornir.core.task import Result, Task
 
 
 def netconf_capabilities(task: Task) -> Result:
+    """
+    Gather Netconf capabilities from device
+    """
     manager = task.host.get_connection("netconf", task.nornir.config)
     capabilities = [capability for capability in manager.server_capabilities]
     return Result(host=task.host, result=capabilities)
-
-
-def test_netconf(netconf):
-    result = netconf.run(netconf_capabilities)
-
-    for _, v in result.items():
-        assert "urn:ietf:params:netconf:capability:writable-running:1.0" in v.result
